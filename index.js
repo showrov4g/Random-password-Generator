142ccb4cdb8afb6377687f07e4e729100913f4c2
showrov4g <showrovg00@gmail.com>
2024-08-25 11:06:32 +0600

    function setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+{}[]/";
+
+const allCars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allCars[Math.floor(Math.random() * allCars.length)];
+    }
+    passwordBox.value = password;
+}
